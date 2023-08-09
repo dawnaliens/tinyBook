b0af22f1ae0f1de1620b0144b179322a1d1d0f8e
Richard Liu <richardliux23@gmail.com>
2023-08-09 16:53:21 +0800

    Try to solve the CORS issue with gin/cors

--- tinyBook-Backend/main.go
+++ tinyBook-Backend/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"strings"
+	"time"
 	"tinyBook/tinyBook-Backend/internal/web"
 )
 
 func main() {
 	server := gin.Default()
+	server.Use(cors.New(cors.Config{
+		// AllowOrigins:  []string{"https://localhost:3000"},
+		//AllowMethods:  []string{"POST"},
+		AllowHeaders: []string{"Content-Type", "Authorization"},
+		// ExposeHeaders: []string{"Content-Type", "Authorization"},
+		// Credentials - ask for bringing cookies
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				// Development environment
+				return true
+			}
+			return strings.Contains(origin, "testing.com")
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 
 	u := web.NewUserHandler()
 	u.RegisterRoutes(server)
